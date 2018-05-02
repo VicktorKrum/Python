0db72907e4c7ec265ab3a7cd1d310004c44fef57
VicktorKrum <sayaniroychowdhury.8@gmail.com>
2018-05-02 18:03:32 +0530

    fruits.py

---
+++ fruits.py
@@ -0,0 +1,7 @@
+fruits = ["apples", "oranges", "mangoes"] # get the list
+length = len(fruits) # get the length that will be needed for the while condition
+i = 0 # initialise a counter
+while i < length: # give the condition
+         print(fruits[i]) # the code block
+         i += 1 # increment the counter
+print(length)
